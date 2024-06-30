154e424155c3070a3a066a0c6f58db40412e08a4
leontwas <140932426+leontwas@users.noreply.github.com>
2024-06-30 19:15:34 -0300

    vinculé el deploy con nuestro Front

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-
 // Obtener el nombre de archivo y directorio actual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,68 +17,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 const PORT = 3000;
 
 // Ruta para servir el archivo index.html
-app.get('/public/index', (req, res) => {
+app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Ruta para servir el archivo productos.html
-app.get('/public/productos', (req, res) => {
+app.get('/productos', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'productos.html'));
 });
 
 // Ruta para servir el archivo novedades.html
-app.get('/public/productos', (req, res) => {
+app.get('/novedades', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'novedades.html'));
 });
 
 // Ruta para servir el archivo reservas.html
-app.get('/public/reservas', (req, res) => {
+app.get('/reservas', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'reservas.html'));
 });
 
-// Ruta para servir el archivo productos.html
-app.get('/public/nosotros', (req, res) => {
+// Ruta para servir el archivo nosotros.html
+app.get('/nosotros', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'nosotros.html'));
-});
-
-// Ruta para servir el archivo fuentes
-app.get('/public/src/css/fuentes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'fuentes.css'));
-});
-
-// Ruta para servir el archivo styleHeader.css
-app.get('/public/src/css/styleHeader', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'stylehHeader.css'));
-});
-
-// Ruta para servir el archivo styleFooter.css
-app.get('/public/src/css/styleHeader', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'stylehFooter.css'));
-});
-
-// Ruta para servir el archivo styleIndex.css
-app.get('/public/src/css/styleIndex', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'styleIndex.css'));
-});
-
-// Ruta para servir el archivo styleProductos.css
-app.get('/public/src/css/styleProductos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'styleProductos.css'));
-});
-
-// Ruta para servir el archivo styleNovedades.css
-app.get('/public/src/css/styleNovedades', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'styleNovedades.css'));
-});
-
-// Ruta para servir el archivo styleReserva.css
-app.get('/public/src/css/styleHeader', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'styleReserva.css'));
-});
-
-// Ruta para servir el archivo styleNosotrtos.css
-app.get('/public/src/css/styleNosotros', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'src', 'css', 'styleNosotros.css'));
 });
 
 app.listen(PORT, () => {
